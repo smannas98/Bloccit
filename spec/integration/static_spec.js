170839669ca1615f2fb36acbc354da94f89eb9ef
smannas98 <seanmannas02@gmail.com>
2019-02-25 14:58:13 -0500

    assignment work for checkpoint 3, added a route handler for the /about route

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -2,6 +2,7 @@ const request = require('request');
 const server = require('../../src/server');
 const base = 'http://localhost:3000';
 const test = '/marco';
+const about = 'http://localhost:3000/about';
 
 describe('routes : static', () => {
     describe('GET /', () => {
@@ -12,12 +13,20 @@ describe('routes : static', () => {
             });
         });
 
-        it('should return status code 200 and string saying "polo"', (done) => {
-            request.get(test, (err, res, body) => {
+       // it('should return status code 200 and string saying "polo"', (done) => {
+        //    request.get(test, (err, res, body) => {
+        //        expect(res.statusCode).toBe(200);
+        //        expect(body).toBe('polo');
+       //         done();
+       //     });
+       // });
+
+        it("should return status code 200 and string saying 'About Us.'", (done) => {
+            request.get(about, (err, res, body) => {
                 expect(res.statusCode).toBe(200);
-                expect(body).toBe('polo');
+                expect(body).toContain('About Us.');
                 done();
-            });
-        });
+            })
+        })
     });
 });
