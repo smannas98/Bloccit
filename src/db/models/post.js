bc17026750b9ff414904aa57e35985726a120356
smannas98 <seanmannas02@gmail.com>
2019-04-09 19:00:26 -0400

    added a lifecycle hook to automatically give a newly created post 1 upvote

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -46,6 +46,13 @@ module.exports = (sequelize, DataTypes) => {
         postId: post.id,
       });
     });
+    Post.afterCreate((post, callback) => {
+      return models.Vote.create({
+        value: 1,
+        userId: post.userId,
+        postId: post.id,
+      });
+    });
   };
   Post.prototype.getPoints = function () {
     if (this.votes.length === 0) {
