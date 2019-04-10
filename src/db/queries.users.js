deb6254034f37a81616e341c9a2a906eb91c7446
smannas98 <seanmannas02@gmail.com>
2019-04-10 15:06:53 -0400

    added profile page for users

--- src/db/queries.users.js
+++ src/db/queries.users.js
@@ -1,5 +1,7 @@
 const bcrypt = require("bcryptjs");
 const User = require("./models").User;
+const Post = require("./models").Post;
+const Comment = require("./models").Comment;
 
 module.exports = {
   createUser(newUser, callback) {
@@ -14,6 +16,31 @@ module.exports = {
     })
     .catch((err) => {
       callback(err);
+    });
+  },
+  getUser(id, callback) {
+    let result = {};
+    User.findById(id)
+    .then((user) => {
+      if (!user) {
+        callback(404);
+      } else {
+        result["user"] = user;
+
+        Post.scope({ method: ["lastFiveFor", id] }).all()
+        .then((posts) => {
+          result["posts"] = posts;
+
+          Comment.scope({ method: ["lastFiveFor", id] }).all()
+          .then((comments) => {
+            result["comments"] = comments;
+            callback(null, result);
+          })
+          .catch((err) => {
+            callback(err);
+          });
+        });
+      }
     });
   },
 };
